bedb7201e92cd4bdbe157f90d462d91c9ecab818
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-11 02:32:37 +0530

    dto

---
+++ auth-service/src/main/java/com/rbhagat32/authservice/dto/AuthResponseDTO.java
@@ -0,0 +1,12 @@
+package com.rbhagat32.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponseDTO {
+
+    private String token;
+    private UserDTO userDTO;
+}
