483c7cd29c18e8769b3aec9c81f7dbbd747c3aa2
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-11 01:49:50 +0530

    api gateway boilerplate

---
+++ api-gateway/src/test/java/com/rbhagat32/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbhagat32.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
