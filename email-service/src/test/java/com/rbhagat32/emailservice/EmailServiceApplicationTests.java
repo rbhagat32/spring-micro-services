8cf61db9554cb5a7e96f31fad0fe026c664fc6bc
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-15 10:30:05 +0530

    email service boilerplate

---
+++ email-service/src/test/java/com/rbhagat32/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbhagat32.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
