7e8bc1059d070a1d320293505d2d0273cef1a994
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-23 10:28:27 +0530

    theme switcher

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "@/context/theme-provider";
+import { ThemeSwitcher } from "@/components/custom/theme-switcher";
+
 export function App() {
-  return <div>app</div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div>
+        <ThemeSwitcher />
+      </div>
+    </ThemeProvider>
+  );
 }
