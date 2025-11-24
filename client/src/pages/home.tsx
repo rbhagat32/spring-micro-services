c1e137fcab0a48cd84f7c1f74e4d02e1a2d8550d
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-24 18:42:11 +0530

    show user info on homepage

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,3 +1,16 @@
+import { type IStore } from "@/types/types";
+import { useSelector } from "react-redux";
+
 export function HomePage() {
-  return <div>HomePage</div>;
+  const user = useSelector((state: IStore) => state.user);
+
+  return (
+    <div>
+      <p>Id: {user.id}</p>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>Created at: {new Date(user.createdAt).toLocaleString()}</p>
+      <p>Updated at: {new Date(user.updatedAt).toLocaleString()}</p>
+    </div>
+  );
 }
