76ec68edcae7b849d2ccecaced1e662c2c2998c4
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-26 18:48:54 +0530

    signup page html

---
+++ client-next/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export default function SignupPage() {
+  return (
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/tendou.jpeg"
+              alt="Image"
+              className="absolute inset-0 h-full w-full rotate-180 object-cover dark:brightness-[0.5]"
+            />
+          </div>
+
+          <form className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome</h1>
+                <p className="text-muted-foreground text-balance">
+                  Create a new <span className="underline">Spring Micro-Services</span> account
+                </p>
+              </div>
+
+              <div className="grid gap-3">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" name="name" type="text" />
+              </div>
+
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" name="email" type="email" />
+              </div>
+
+              <div className="grid gap-3">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" name="password" type="password" />
+              </div>
+
+              <Button type="submit" className="w-full" disabled={true}>
+                Sign up
+              </Button>
+
+              <div className="text-center text-sm">
+                Already have an account?{" "}
+                <Link href="/login" className="underline underline-offset-4">
+                  Log in
+                </Link>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
