74a77bb2bc05987741feedf1b1d4b6fcef2805c9
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-27 20:28:18 +0530

    signup logic complete

--- client-next/src/app/(auth)/signup/page.tsx
+++ client-next/src/app/(auth)/signup/page.tsx
@@ -1,9 +1,6 @@
-import { Button } from "@/components/ui/button";
+import { SignupForm } from "@/components/core/signup-form";
 import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function SignupPage() {
   return (
@@ -21,42 +18,7 @@ export default function SignupPage() {
             />
           </div>
 
-          <form className="p-6 md:p-8">
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome</h1>
-                <p className="text-muted-foreground text-balance">
-                  Create a new <span className="underline">Spring Micro-Services</span> account
-                </p>
-              </div>
-
-              <div className="grid gap-3">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" name="name" type="text" />
-              </div>
-
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" name="email" type="email" />
-              </div>
-
-              <div className="grid gap-3">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" name="password" type="password" />
-              </div>
-
-              <Button type="submit" className="w-full" disabled={true}>
-                Sign up
-              </Button>
-
-              <div className="text-center text-sm">
-                Already have an account?{" "}
-                <Link href="/login" className="underline underline-offset-4">
-                  Log in
-                </Link>
-              </div>
-            </div>
-          </form>
+          <SignupForm />
         </CardContent>
       </Card>
     </div>
---
+++ client-next/src/components/core/signup-form.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { signupAction } from "@/actions/mutation/signup";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useActionState, useEffect, useState, useTransition } from "react";
+import { toast } from "sonner";
+
+export function SignupForm() {
+  const router = useRouter();
+  const [_isPending, startTransition] = useTransition();
+
+  const [state, formAction] = useActionState(signupAction, null);
+  const [loading, setLoading] = useState(false);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData(e.currentTarget);
+
+    startTransition(() => {
+      formAction(formData);
+    });
+  }
+
+  useEffect(() => {
+    if (!state) return;
+
+    // zod validation errors
+    if (state.zodErrors) {
+      Object.values(state.zodErrors).forEach((errors) => {
+        if (errors) {
+          errors.forEach((err) => toast.error(err));
+        }
+      });
+      setLoading(false);
+      return;
+    }
+
+    // server errors
+    if (state.success === false && state.message) {
+      toast.error(state.message);
+      setLoading(false);
+      return;
+    }
+
+    // success
+    if (state.success) {
+      toast.success("Login successful!");
+      router.push("/");
+    }
+
+    setLoading(false);
+  }, [state]);
+
+  return (
+    <form onSubmit={handleSubmit} className="p-6 md:p-8">
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-2xl font-bold">Welcome</h1>
+          <p className="text-muted-foreground text-balance">
+            Create a new <span className="underline">Spring Micro-Services</span> account
+          </p>
+        </div>
+
+        <div className="grid gap-3">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" name="name" />
+        </div>
+
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" name="email" />
+        </div>
+
+        <div className="grid gap-3">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" name="password" type="password" />
+        </div>
+
+        <Button type="submit" className="w-full" disabled={loading}>
+          Sign up
+        </Button>
+
+        <div className="text-center text-sm">
+          Already have an account?{" "}
+          <Link href="/login" className="underline underline-offset-4">
+            Log in
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+}
